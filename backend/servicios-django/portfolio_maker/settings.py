a4a404f2e5dc4b7b7b89269d89b6d5492c8e0d09
elterribleabuelo <rguerreroh@uni.pe>
2022-05-13 22:36:09 -0500

    views login

--- backend/servicios-django/portfolio_maker/settings.py
+++ backend/servicios-django/portfolio_maker/settings.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import  os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = BASE_DIR / 'portfolio_maker'
 
 
 # Quick-start development settings - unsuitable for production
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'portfolio_maker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- backend/servicios-django/user_interface/views.py
+++ backend/servicios-django/user_interface/views.py
@@ -1,7 +1,64 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from audioop import reverse
+from msilib import sequence
+from django.contrib.auth import authenticate,login,logout
+from django.db import IntegrityError
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404, HttpResponseRedirect,JsonResponse
+from .models import (User,InformationModel,EducationModel,ExperienceModel,SkillsetModel,ProjectModel,MessageModel)
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request,template_name = "user_interface/index.html")
+    #return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def login_view(request, *args,**kwargs):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username,password)
+
+        # Verificamos si la autenticacion es exitosa
+        if user is not None:
+            login(request,user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, template_name = "user_interface/login.html",context = {"message":"Usuario o password inválidos.Porfavor introduzca sus credenciales de nuevo"})
+    else:
+        return render(request, template_name = "user_interface/login.html",context = {"message":""})
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        email = request.POST["email"]
+        
+        # Comparando las claves escritas
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        
+        if password != confirmation:
+            return render(request, template_name = "user_interface/register.html",context = {"message":"Las contraseñas no coinciden.Intente de nuevo"})
+
+        # Creando el nuevo usuario
+        try:
+            user = User.objects.create_user(username,email,password)
+            user.save()
+        except IntegrityError:
+            return render(request, template_name = "user_interface/register.html",context = {"message":"El usuario ya existe"})
+
+        login(request,user)
+        
+        return HttpResponseRedirect(reverse("index"))
+    
+    else:
+        return render(request, template_name = "user_interface/register.html")
+        
+        
+    
+    
